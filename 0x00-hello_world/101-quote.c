5e4ce251037ee3f7e38b273d5ebce6e557550900
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-06-02 17:54:34 -0700

    New commit on UNIX Operating Sysrem

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
+}
