a60d6ad0c2a1d81dbd3de6a820a427f70f941c55
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-06-27 07:38:20 -0700

    New changes, git commit to Write a function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,13 @@
-#include "main.h"
-
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: nothing, return void
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+	return (write(1, &c, 1));
 }
-
