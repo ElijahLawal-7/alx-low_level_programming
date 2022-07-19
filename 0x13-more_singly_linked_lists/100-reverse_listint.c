9aedadfe7d03611422372aaa9269d4e0024a4816
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-07-19 02:13:13 -0700

    A Program to Write a function that reverses a listint_t linked list.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,47 +1,25 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * of a linked list.
- * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
  *
- * Return: 1 if it succeeded, -1 if it failed.
+ * Return: pointer to the first node in the new list
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *prev;
-	listint_t *next;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	prev = *head;
-
-	if (index != 0)
+	while (*head)
 	{
-		for (i = 0; i < index - 1 && prev != NULL; i++)
-		{
-			prev = prev->next;
-		}
-	}
-
-	if (prev == NULL || (prev->next == NULL && index != 0))
-	{
-		return (-1);
-	}
-
-	next = prev->next;
-
-	if (index != 0)
-	{
-		prev->next = next->next;
-		free(next);
-	}
-	else
-	{
-		free(prev);
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
 		*head = next;
 	}
 
-	return (1);
+	*head = prev;
+
+	return (*head);
 }
