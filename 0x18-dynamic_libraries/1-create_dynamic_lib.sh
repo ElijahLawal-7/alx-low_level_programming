0c8b7ad8a2d2b39681befa7e160157574a654d8b
Ismaila Lawal Elijah <2017en0285@unijos.edu.ng>
2022-09-09 20:46:05 -0700

    dynamic lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
 gcc -shared -Wl,-soname,liball.so -o liball.so *.o
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
-#ifndef _HOLBERTON_
-#define _HOLBERTON_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
